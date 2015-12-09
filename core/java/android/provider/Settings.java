omnirom/android_frameworks_base_old

(1/2) base: Enable voice lockscreen shortcut
Change-Id: I521fdb53a45776bda81d9598f3cc5fb310fbf8d1

core/java/android/provider/Settings.java
@@ -5003,6 +5003,12 @@ public final class Settings {
         public static final String KEYGUARD_QUICK_UNLOCK = "keyguard_quick_unlock";
 
         /**
+         * Enable voice lockscreen shortcut
+         * @hide
+         */
+        public static final String LOCKSCREEN_VOICE_UNLOCK = "lockscreen_voice_unlock";
+
+        /**
          * Whether lock pattern will vibrate as user enters (0 = false, 1 =
          * true)
          *

Omniherna/android_frameworks_base

core/java/android/provider/Settings.java
@@ -3726,6 +3726,12 @@ public final class Settings {
         public static final String RECENTS_SHOW_SEARCH_BAR = "recents_show_search_bar";
 
         /**
+         * Enable voice lockscreen shortcut
+         * @hide
+         */
+        public static final String LOCKSCREEN_VOICE_SHORTCUT = "lockscreen_voice_shortcut";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *